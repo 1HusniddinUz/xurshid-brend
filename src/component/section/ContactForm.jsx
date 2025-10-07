eacdf139ff645f1f48760ee8606430c73bf1ac14
1HusniddinUz <husniddinhalimov112@gmail.com>
2025-10-07 10:59:14 +0500

    location changed

--- src/component/section/ContactForm.jsx
+++ src/component/section/ContactForm.jsx
@@ -70,13 +70,13 @@ const ContactForm = () => {
                 <div className="contactLocation">
                     <h3>location</h3>
                     <iframe
-                        title="map"
-                        src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d6115.257172022556!2d64.51145!3d39.972056!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMznCsDU4JzE5LjQiTiA2NMKwMzAnNDEuMiJF!5e0!3m2!1sru!2s!4v1758601992065!5m2!1sru!2s"
-                        width="100%"
-                        height="450"
-                        style={{
-                            border: "none",
-                            borderRadius: "10px",
+                            title="map"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d191.55146678680578!2d64.43875920688576!3d39.81095143753254!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f500f3192a49b83%3A0xe871868d890d6134!2z4oCcU2l0b3JhaSBNb2hpIFhvc2HigJ0gc2FuYXRvcml5c2kgfCDQodCw0L3QsNGC0L7RgNC40LkgwqvQodC40YLQvtGA0LDQuCDQnNC-0YXQuCDQpdC-0YHQsMK7!5e0!3m2!1sru!2s!4v1759816684040!5m2!1sru!2s"
+                            width="100%"
+                            height="450"
+                            style={{
+                                border: "none",
+                                borderRadius: "10px",
                             boxShadow: "0 4px 10px rgba(0, 0, 0, 0.4)",
                         }}
                         allowFullScreen=""
